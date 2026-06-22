a91c879665ecd6f55d825cbd5333b45bf2c5f6e7
Lucas Vilela <lrvilelab@hotmail.com>
2026-06-22 11:34:58 -0300

    settings connection DB with prisma management (+ generate prisma client) and  create user registration init

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import { routes } from './routes'
 
 const app = express()
 
-app.use(express())
+app.use(express.json())
 app.use(routes)
 app.use(errorHandling)
 
--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,7 +1,18 @@
 import { Request, Response } from "express";
+import { z } from 'zod'
 
 export class UsersController {
   async create(request: Request, response: Response){
-    return response.json("Rota users OK!")
+    const bodySchema = z.object({
+      name: z.string(),
+      email: z.email(),
+      password: z.string().min(6),
+    })
+
+    const { name, email, password } = bodySchema.parse(request.body)
+
+    const verifyEmailExists = 
+
+    return response.json({ name, email, password })
   }
 }
