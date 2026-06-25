593c0b1a9cb968d7c415244bcab1328cfdbca3a0
Lucas Vilela <lrvilelab@hotmail.com>
2026-06-25 11:01:55 -0300

    added function 'create' memberTeam to controller 'team-member-controller.ts'

--- src/controllers/team-member-controller.ts
+++ src/controllers/team-member-controller.ts
@@ -1,7 +1,23 @@
 import { Request, Response } from "express";
+import { z } from 'zod'
+import { prisma } from "@/database/prisma";
 
 export class TeamMemberController{
   async create(req: Request, res: Response){
-    return res.json('ok')
+    const bodySchema = z.object({
+      user_id: z.uuid(),
+      team_id: z.uuid()
+    })
+
+    const { user_id, team_id } = bodySchema.parse(req.body)
+
+    const member = await prisma.teamMember.create({
+      data: {
+        userId: user_id,
+        teamId: team_id
+      }
+    })
+
+    return res.status(201).json(member)
   }
 }
