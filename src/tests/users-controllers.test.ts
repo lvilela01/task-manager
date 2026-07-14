4b865438ee1142a6dbc2f444bf52dea8aca18bb4
Lucas Vilela <lrvilelab@hotmail.com>
2026-07-14 11:17:13 -0300

    added function delete user after runned test

--- src/tests/users-controllers.test.ts
+++ src/tests/users-controllers.test.ts
@@ -1,9 +1,19 @@
-import { describe, expect, it } from "@jest/globals"
+import { afterAll, describe, expect, it } from "@jest/globals"
 import request from "supertest"
+import { prisma } from "@/database/prisma.js"
 
 import { app } from "@/app.js"
-
 describe("UsersController", () => {
+  let user_id: string
+
+  afterAll(async () => {
+    if(user_id){
+      await prisma.user.delete({ where: { id: user_id } })
+    } else {
+      console.warn("afterAll executado, mas 'user_id' estava indefinido!")
+    }
+  })
+
   it("should create a new user successfully", async () => {
     const response = await request(app).post('/users').send({
       name: "Test User",
@@ -17,7 +27,9 @@ describe("UsersController", () => {
       user: expect.objectContaining({
         name: "Test User",
         email: "testuser@example.com",
-      }),
+      })
     })
+
+    user_id = response.body.id || response.body.user?.id
   })
 })
